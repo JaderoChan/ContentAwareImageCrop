2fb2c895042f157381ed1b16286adac2949f51e3
JaderoChan <c_dl_cn@outlook.com>
2026-03-12 00:15:47 +0800

    update frontend

--- frontend/src/main.cpp
+++ frontend/src/main.cpp
@@ -1,10 +1,37 @@
 #include <qapplication.h>
 
+#include <easy_translate.hpp>
+
+#include <config.h>
+#include <trwidgets/trwidget.h>
+
+#include "logo_icon.h"
+#include "settings.h"
+
 int main(int argc, char* argv[])
 {
+    // 设置程序全局属性
     QApplication a(argc, argv);
+    a.setOrganizationDomain(APP_ORGANIZATION_DOMAIN);
+    a.setOrganizationName(APP_ORGANIZATION);
+    a.setApplicationName(EASYTR(APP_TITLE));
+    a.setApplicationDisplayName(EASYTR(APP_TITLE));
+    a.setApplicationVersion(APP_VERSION);
+    a.setWindowIcon(getLogoIcon());
+
+    // 设置语言
+    {
+        Settings settings = loadSettings();
+        setLanguage(settings.language);
+    }
+
+    TrWidget wgt;
+    wgt.show();
 
     int ret = a.exec();
+
+    // 更新翻译文件（实际上由编译选项 `UPDATE_TRANSLATIONS_FILES` 决定是否真正更新）
+    easytr::updateTranslationsFiles();
 
     return ret;
 }
